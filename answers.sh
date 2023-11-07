576ef7fef37e33bf916413295cca31982645d646
David Gwartney <david.gwartney@kore.com>
2023-11-07 10:05:14 -0800

    check

--- answers.sh
+++ answers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 
-curl --location --request POST "https://searchassist.kore.ai/searchassistapi/public/searchAssist/stream/${APP_ID}/${SEARCH_ID}/fullSearch" \
+curl -q --location --request POST "https://${API_HOST}/searchassistapi/public/searchAssist/stream/${APP_ID}/${SEARCH_ID}/fullSearch" \
 --header "auth: ${SEARCH_ASSIST_JWT_TOKEN}" \
 --header 'Content-Type: application/json' \
---data-raw "{ \"query\": \"${1}\", \"maxNumOfResults\":4 }"   | jq .template.graph_answer
+--data-raw "{ \"query\": \"${1}\", \"maxNumOfResults\":4 }" | jq .template.graph_answer
 
 
--- get-pages.sh
+++ get-pages.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-#set -x
+set -x
 
 mkdir -p html
 
 for url in $(cat "$1")
 do
+  # Remove trailing slash from the URL if present
+  url=${url%/}
+  echo $url
 
-
-# Remove trailing slash from the URL if present
-url=${url%/}
-echo $url
-
-# Extract the last path element using awk and print it
-path_element=$(echo "$url" | awk -F'/' '{print $NF}')
-curl -L --header "user-agent: Mozilla/5.0" $url -o html/${path_element}.html
-sleep 0
+  # Extract the last path element using awk and print it
+  path_element=$(echo "$url" | awk -F'/' '{print $NF}')
+  curl -L --header "user-agent: Mozilla/5.0" $url -o html/${path_element}.html
+  sleep 0
 
 done 
